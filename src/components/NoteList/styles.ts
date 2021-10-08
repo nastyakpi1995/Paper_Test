c84d4ea6b9dead0f5bde54055dbda6f227d58af1
Nastya Krat <nastyakpi1995@gmail.com>
2021-10-08 12:00:02 +0300

    add styles

--- src/components/NoteList/styles.ts
+++ src/components/NoteList/styles.ts
@@ -1,27 +1,41 @@
 interface IStyle {
-    border: string,
-    paddingLeft: number,
-    paddingRight: number,
-    marginTop: number,
-    margin: number,
-    display: string,
-    flexDirection: 'row',
-    justifyContent: string
+    paddingLeft?: number,
+    paddingRight?: number,
+    marginTop?: number,
+    margin?: number,
+    display?: string,
+    flexDirection?: 'row',
+    justifyContent?: string;
+    background?: string;
+    padding?: number;
+    borderBottom?: string;
+    width?: number;
+    height?: number | string;
 }
 
 interface IStyles {
-    mainWrap: IStyle
+    mainWrap: IStyle;
+    list: IStyle;
 }
 
 export const styles: IStyles = {
     mainWrap: {
-        border: '1px solid',
+        background: '#ececec',
         paddingLeft: 420,
         paddingRight: 420,
         marginTop: 20,
         margin: 20,
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-between'
-    }
+        justifyContent: 'space-between',
+        height: '100vh',
+    },
+    list: {
+        background: 'white',
+        paddingRight: 49,
+        paddingLeft: 19,
+        width: 350,
+        borderBottom: '1px solid grey',
+        marginTop: 5
+    },
 }
